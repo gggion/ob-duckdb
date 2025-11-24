144f1f0a1f3b311ad63525ea193ffc8b0d52b9dc
gggion <gggion123@gmail.com>
2025-11-24 00:49:10 -0300

    Add GPL-3.0-or-later license headers per melpa/melpa#9693
    * ob-duckdb.el: Add SPDX-License-Identifier and GPL-3.0-or-later license
    boilerplate.
    
    * ob-duckdb-blocks.el: Add SPDX-License-Identifier and GPL-3.0-or-later
    license boilerplate.
    
    Both files now include proper license text as required by MELPA
    submission guidelines. License is GPL version 3 or later.

--- ob-duckdb-blocks.el
+++ ob-duckdb-blocks.el
@@ -5,6 +5,21 @@
 ;; Keywords: org, duckdb, data
 ;; URL: https://github.com/gggion/ob-duckdb
 
+;; SPDX-License-Identifier: GPL-3.0-or-later
+
+;; This file is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published
+;; by the Free Software Foundation, either version 3 of the License,
+;; or (at your option) any later version.
+;;
+;; This file is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this file.  If not, see <https://www.gnu.org/licenses/>.
+
 ;;; Commentary:
 ;;
 ;; This ob-duckdb addon tracks DuckDB source blocks in Org mode documents,
